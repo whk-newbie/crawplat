27cd29fedbb02292ecb470f3402f1cb294d32ee2
whk <15170188941@163.com>
2026-05-05 14:44:51 +0800

    feat: add web i18n core

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,14 +1,30 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useLocaleStore } from './stores/locale'
+
+const localeStore = useLocaleStore()
+const { availableLocales, locale } = storeToRefs(localeStore)
+</script>
+
 <template>
   <div>
     <header>
-      <nav>
-        <router-link to="/login">Login</router-link>
-        <router-link to="/projects">Projects</router-link>
-        <router-link to="/spiders">Spiders</router-link>
-        <router-link to="/executions">Executions</router-link>
-        <router-link to="/monitor">Monitor</router-link>
-        <router-link to="/datasources">Datasources</router-link>
+      <nav aria-label="Main navigation">
+        <router-link to="/login">{{ localeStore.t('navigation.login') }}</router-link>
+        <router-link to="/projects">{{ localeStore.t('navigation.projects') }}</router-link>
+        <router-link to="/spiders">{{ localeStore.t('navigation.spiders') }}</router-link>
+        <router-link to="/executions">{{ localeStore.t('navigation.executions') }}</router-link>
+        <router-link to="/monitor">{{ localeStore.t('navigation.monitor') }}</router-link>
+        <router-link to="/datasources">{{ localeStore.t('navigation.datasources') }}</router-link>
       </nav>
+      <label>
+        {{ localeStore.t('app.language') }}
+        <select :value="locale" @change="localeStore.setLocale(($event.target as HTMLSelectElement).value as 'zh-CN' | 'en-US')">
+          <option v-for="item in availableLocales" :key="item" :value="item">
+            {{ item }}
+          </option>
+        </select>
+      </label>
     </header>
     <router-view />
   </div>
